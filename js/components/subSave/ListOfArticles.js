c8387a3215fd867e3eac75d3f3ad54c5ff5ee1a8
musti-91 <mustafa.alroomi@cronos.be>
2018-03-24 21:25:38 +0100

    edit some function to get data from firebase

--- js/components/subSave/ListOfArticles.js
+++ js/components/subSave/ListOfArticles.js
@@ -1,12 +1,21 @@
 import Axios from "axios";
-
+import PerfectScrollbar from "perfect-scrollbar";
 export default class ListOfArticles {
   constructor(item) {
     this.item = item;
     this.deleteButton = "";
     this.listHolder = "";
+    this.ps = "";
+    this.addScrollbar();
     this.init();
     this.eventsListener();
+  }
+  addScrollbar() {
+    this.ps = new PerfectScrollbar("#listHolder", {
+      wheelSpeed: 2,
+      wheelPropagation: false,
+      minScrollbarLength: 10
+    });
   }
   init() {
     let el = `<li>`;
@@ -16,7 +25,7 @@ export default class ListOfArticles {
       .then(response => {
         response.data.response.items.forEach(element => {
           el += `<img src='${element.image.full}'>
-                <h3>${element.title.replace(/"/g, "")}</h3>
+          <h3>${element.title.replace(/"/g, "")}</h3>
                 <span id="trash_delete"></span>`;
         });
         el += `</li>`;
@@ -26,6 +35,7 @@ export default class ListOfArticles {
       })
       .catch("Something went Wrong!");
     this.listHolder = document.getElementById("listHolder");
+    this.ps.update();
     this.deleteButton = document.getElementById("trash_delete");
   }
   eventsListener() {}
--- js/components/subSearch/SearchResults.js
+++ js/components/subSearch/SearchResults.js
@@ -1,22 +1,33 @@
 import Axios from "axios";
 import FavouriteItem from "./FavouriteItem";
-
+import PerfectScrollbar from "perfect-scrollbar";
 export default class SearchResults {
   constructor(item, articleHolder, saveArticle) {
     this.item = item;
     this.saveArticle = saveArticle;
     this.articleHolder = articleHolder;
     this.favItem = "";
+    this.ps = "";
+    this.addScrollbar();
     this.createResult();
     this.events();
   }
+  addScrollbar() {
+    this.ps = new PerfectScrollbar(this.articleHolder.parentElement, {
+      wheelSpeed: 2,
+      wheelPropagation: false,
+      minScrollbarLength: 10
+    });
+  }
   createResult() {
     let el = `<li>`;
-    el += `<h3>${this.item.title}</h3>
+    el += ` <h3>${this.item.title}</h3>
             <p>${this.item.snippets.content}</p>
-           <span id="${this.item.fields.entity_id}"></span></li>`;
+            <span id="${this.item.fields.entity_id}"></span>
+           </li>`;
     this.articleHolder.insertAdjacentHTML("beforeend", el);
     this.favItem = document.getElementById(`${this.item.fields.entity_id}`);
+    this.ps.update();
   }
   events() {
     this.favItem.addEventListener("click", e => {
