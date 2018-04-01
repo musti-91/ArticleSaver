7be5a2e4d173453e1b01ca7590f739e88bb589d4
musti-91 <mustafa.alroomi@cronos.be>
2018-04-01 16:18:59 +0200

    add suggest box & add validation for input

--- js/components/SearchArticle.js
+++ js/components/SearchArticle.js
@@ -1,5 +1,7 @@
 import SearchResults from "./subSearch/SearchResults";
 import Axios from "axios";
+import Suggest from "./subSearch/Suggest";
+import SuggestBox from "./subSearch/SuggestBox";
 
 export default class SearchArticle {
   constructor(holder, savedArticlesList, firebaseRef) {
@@ -14,7 +16,7 @@ export default class SearchArticle {
   createHTML() {
     let el = `<h2 class="title">Search Article</h2><hr>`;
     el += `<form id="form">
-            <input type="text" id="in_val" class="input" autocomplete="off">
+            <input type="text" id="in_val" class="input" autocomplete="off" autofocus>
             <button class="button">Search</button>
           </form>
           <ul id="resultsHolder" data-scrollbar></ul>
@@ -27,22 +29,39 @@ export default class SearchArticle {
     this.holder.querySelector("form").addEventListener("submit", e => {
       e.preventDefault();
       this.articleHolder.innerHTML = "";
-      Axios.get(
-        `https://nieuws.vtm.be/feed/articles/solr?format=json&query=${
-          this.input.value
-        }`
-      )
-        .then(response => {
-          response.data.response.items.forEach(item => {
-            new SearchResults(
-              item,
-              this.articleHolder,
-              this.savedArticlesList,
-              this.firebaseRef
-            );
-          });
-        })
-        .catch("Error!");
+      if (this.validate(this.input)) {
+        Axios.get(
+          `https://nieuws.vtm.be/feed/articles/solr?format=json&query=${
+            this.input.value
+          }`
+        )
+          .then(response => {
+            response.data.response.items.forEach(item => {
+              new SearchResults(
+                item,
+                this.articleHolder,
+                this.savedArticlesList,
+                this.firebaseRef
+              );
+            });
+          })
+          .catch("Error!");
+      }
     });
+    this.input.addEventListener("input", () => {
+      this.articleHolder.innerHTML = "";
+      this.input.classList.remove("error");
+      if (this.validate(this.input)) {
+        new SuggestBox(this.input, this.articleHolder);
+      }
+    });
+  }
+  validate(input) {
+    if (input.value.length <= 2) {
+      this.input.classList.add("error");
+      return false;
+    }
+    this.input.classList.remove("error");
+    return true;
   }
 }
---
+++ js/components/subSearch/SuggestBox.js
@@ -0,0 +1,91 @@
+import Axios from "axios";
+
+export default class SuggestBox {
+  constructor(input, holder) {
+    this.input = input;
+    this.holder = holder;
+    this.suggestions = [];
+    this.nr = 0;
+    this.selectIndex = 0;
+    this.liElement = "";
+    this.getData();
+    this.events();
+  }
+  getData() {
+    Axios.get(
+      `https://nieuws.vtm.be/feed/articles/solr?format=json&query=${
+        this.input.value
+      }`
+    )
+      .then(response => {
+        const data = response.data.response;
+        data.items.forEach(item => {
+          this.suggestions.push(item);
+        });
+        this.createHtml();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  createHtml() {
+    let html = "";
+    this.holder.innerHTML = "";
+    this.suggestions.forEach(element => {
+      this.nr++;
+      html += `<li id="suggestItem-${this.nr}">
+                    <h4>${element.title}</h4>
+          </li>`;
+    });
+    this.holder.insertAdjacentHTML("beforeend", html);
+    this.liElement = document.getElementById(`suggestItem-${this.nr}`);
+  }
+  events() {
+    this.holder.addEventListener("click", this.handleClick.bind(this));
+    this.input.addEventListener("keydown", this.handleKeydown.bind(this));
+  }
+  handleClick(e) {
+    if (e.target.nodeName == "H4") {
+      this.choose(e.target.innerHTML);
+    }
+  }
+  handleKeydown(e) {
+    if (e.keyCode == 38 || e.keyCode == 40 || e.keyCode == 13) {
+      if (e.keyCode == 13) {
+        this.choose(
+          document.getElementById(`suggestItem-${this.nr}`).childNodes[1]
+            .innerHTML
+        );
+      }
+      if (e.keyCode == 40) {
+        this.selectIndex++;
+        if (this.holder.querySelector("li.chosen")) {
+          this.holder.querySelector("li.chosen").classList.remove("chosen");
+        }
+        this.holder
+          .querySelector(`li:nth-child(${this.selectIndex})`)
+          .classList.add("chosen");
+        if (this.selectIndex == this.holder.childElementCount) {
+          this.selectIndex = 0;
+        }
+      }
+      if (e.keyCode == 38) {
+        this.selectIndex--;
+        if (this.holder.querySelector("li.chosen")) {
+          this.holder.querySelector("li.chosen").classList.remove("chosen");
+        }
+        this.holder
+          .querySelector(`li:nth-child(${this.selectIndex})`)
+          .classList.add("chosen");
+        if (this.selectIndex == 1) {
+          this.selectIndex = this.holder.childElementCount + 1;
+        }
+      }
+    }
+  }
+  choose(value) {
+    this.input.value = value;
+    this.holder.innerHTML = "";
+    this.selectIndex = 0;
+  }
+}
