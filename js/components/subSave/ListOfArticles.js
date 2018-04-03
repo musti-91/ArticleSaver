3535c5ed63f78cc0784acb183e34fe2f26781afc
musti-91 <mustafa.alroomi@cronos.be>
2018-04-03 20:14:09 +0200

    add read time, with its styles

--- js/components/subSave/ListOfArticles.js
+++ js/components/subSave/ListOfArticles.js
@@ -5,26 +5,46 @@ export default class ListOfArticles {
     this.id = id;
     this.firebaseRef = firebaseRef;
     this.holder = holder;
+    this.date = "";
     this.article = "";
     this.getArticle();
   }
   getArticle() {
+    this.startDate = Date.now();
     Scrollbar.init(document.getElementById("listHolder"));
-    Axios.get(`https://nieuws.vtm.be/feed/articles?format=json&ids=${this.id}`)
+    Axios.get(
+      `https://nieuws.vtm.be/feed/articles?format=json&fields=html&ids=${
+        this.id
+      }`
+    )
       .then(response => {
         this.article = response.data.response.items[0];
+        this.artcileText = this.article.text_html;
         this.init();
       })
       .catch(error => {
         console.log(error);
       });
   }
+  // .replace(/"/g, "")
   init() {
     let el = `<li data-id="${this.article.id}" class="animated rollIn">`;
-    el += `<img src='${this.article.image.full}'>
-                <h3>${this.article.title.replace(/"/g, "")}</h3>
+    el += `     <img src='${this.article.image.full}'>
+                <h3>${this.article.title}
+                  </br>
+                  <p id="timer">${this.clcualteReadTime()} read</p>
+                </h3>
                 <span id="trash_delete"></span>`;
     el += `</li>`;
     this.holder.insertAdjacentHTML("beforeend", el); // ul
+  }
+  clcualteReadTime() {
+    let readTime = Math.floor(this.artcileText.length / 60);
+    readTime = readTime.toString();
+    if (readTime.startsWith("0")) {
+      return readTime.substring(0, readTime.length - 1) + " sec";
+    } else {
+      return readTime.substring(0, readTime.length - 1) + " min";
+    }
   }
 }
